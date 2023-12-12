4da13f8beb5eba5fae29264c2356b111e830f148
Chad Boyce <chadboyce@gmail.com>
2023-12-12 14:05:41 +0000

    docs WIP

---
+++ docs/src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+  let code= 'code';
+</script>
+
+<div class="h-screen w-full bg-gradient-to-b from-gray-700 via-gray-900 to-black">
+<header class="p-4">
+ <h1 class="inline-block bg-gradient-to-tr from-orange-500 to-yellow-300 bg-clip-text text-5xl text-transparent">bookmarklets</h1>
+</header>
+
+<div class="mockup-code">
+  <pre data-prefix="~"><code>{code}</code></pre>
+</div>
+
+
+<div class="mockup-window border bg-base-300">
+  <div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+</div>
+
+
+<div class="suckmockup-browser border border-base-300">
+	<div class="suckmockup-browser-toolbar">
+		<div class="suckinput border border-base-300">https://daisyui.com</div>
+	</div>
+	<div class="flex justify-center px-4 py-16 border-t border-base-300">Hello!</div>
+</div>
+
+<div class="mockup-browser border bg-base-300">
+	<div class="mockup-browser-toolbar">
+		<div class="input">https://daisyui.com</div>
+	</div>
+	<div class="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
+</div>
+
+</div>
